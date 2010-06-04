1b3ddd0a3006d4337297dc09c780911af2f73b1a
Michael <mcsulliv@uwaterloo.ca>
2010-06-04 12:01:27 -0400

    Added a Pages controller

---
+++ scriptt/server/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '037abd8a35d67c2f49a4ef29e6ce43a925236d47341378dfff303f6d4fce8dba1330b914acb90c2fde60ec2b3f9404db128f098cf648da69f0750e8cbdbb8a01';
---
+++ scriptt/server/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_server_session',
+  :secret      => 'ec8991a368264f0755309307084f41a8928692b5b28d436a5df4dd889d094c7789f1dea62509aacc99a48184090b308f6a5f9244cc14bbd943379cf2ccf7fb86'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
